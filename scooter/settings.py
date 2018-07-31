98ea8c2cc557772c0b05e9596121aac0d2262ecb
Søren Lind Kristiansen <soren@gutsandglory.dk>
2018-07-31 13:31:32 +0200

    Tweak sleep times

--- scooter/settings.py
+++ scooter/settings.py
@@ -8,10 +8,10 @@ REDIS_HOST = os.environ.get('SCOOTER_REDIS_HOST', 'localhost')
 REDIS_QUEUE = os.environ.get('SCOOTER_REDIS_QUEUE', 'prediction:queue')
 
 # The amount of time the web threads sleep before looking for completed prediction.
-WEB_SLEEP = float(os.environ.get('SCOOTER_WEB_SLEEP', "0.15"))
+WEB_SLEEP = float(os.environ.get('SCOOTER_WEB_SLEEP', "0.10"))
 
 # The amount of time the worker sleeps before looking for more work.
-WORKER_SLEEP = float(os.environ.get('SCOOTER_WORKER_SLEEP', "0.15"))
+WORKER_SLEEP = float(os.environ.get('SCOOTER_WORKER_SLEEP', "0.05"))
 
 # Maximum batch size used when making predictions.
 BATCH_SIZE = int(os.environ.get('SCOOTER_BATCH_SIZE', "16"))
